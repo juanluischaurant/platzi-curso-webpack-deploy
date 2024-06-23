d490036e474149d12e4343e2079bcaacb3c1a7e5
Juan Luis Chaurant <juanluischaurant@gmail.com>
2024-06-23 10:46:02 -0400

    Update create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -3,4 +3,4 @@ const fs = require('fs')
 // la variable process.env.API viene de Netlify ahora ahora
 // estas variables se pueden definir en el servidor
 // y Nodejs las accesa.
-fs.writeFileSync('./.env', `API=%${process.env.API}\n`)
+fs.writeFileSync('./.env', `API=${process.env.API}\n`)
